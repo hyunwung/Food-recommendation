b89e4a8657d367bbb963bbcc97d4c05cb0a633c7
hyunwungSEO <79622280+hyunwung@users.noreply.github.com>
2022-06-24 14:01:50 +0900

    2022-06-24 서현웅 커밋

--- server/node/login/src/js/weather.js
+++ server/node/login/src/js/weather.js
@@ -1,20 +1,34 @@
 let API_KEY = '8b2edef7ea3ca882a95d2761906d0a33'
 
-function onGeoSuccess(position){
-    let lat = position.coords.latitude;
-    let lng = position.coords.longitude;
+function onGeoSuccess(){
+    let lng = 126.977829
+    let lat = 37.56826
     let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`;
+    console.log(url);
     fetch(url)
         .then(response => response.json())
         .then((data)=>{
             let weather = document.querySelector("#weather span:first-child");
             let city = document.querySelector("#weather span:last-child");
             console.log(weather, city)
-            weather.innerText = `${data.weather[0].description} / ${data.main.temp}°C`
+            weather.innerText = `${data.weather[0].main} / ${data.main.temp}°C`
         })
 }
-navigator.geolocation.getCurrentPosition(onGeoSuccess,()=>{console.log('fail')});
+onGeoSuccess();
 
+// let API_KEY = '8b2edef7ea3ca882a95d2761906d0a33'
 
-
-
+// function onGeoSuccess(position){
+//     let lat = position.coords.latitude;
+//     let lng = position.coords.longitude;
+//     let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`;
+//     fetch(url)
+//         .then(response => response.json())
+//         .then((data)=>{
+//             let weather = document.querySelector("#weather span:first-child");
+//             let city = document.querySelector("#weather span:last-child");
+//             console.log(weather, city)
+//             weather.innerText = `${data.weather[0].description} / ${data.main.temp}°C`
+//         })
+// }
+// navigator.geolocation.getCurrentPosition(onGeoSuccess,()=>{console.log('fail')});
