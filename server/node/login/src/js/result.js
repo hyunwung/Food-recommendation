288f2337691b5a6cff43bd48a7a2909fbc5dc473
hyunwungSEO <79622280+hyunwung@users.noreply.github.com>
2022-06-28 14:34:31 +0900

    2022-06-28 커밋

--- server/node/login/src/js/result.js
+++ server/node/login/src/js/result.js
@@ -61,31 +61,6 @@ const images5={
 const date = new Date();
 const hours = date.getHours();
 
-window.onload = function () {
-    let canvas = document.getElementById("canv");
-    let context = canvas.getContext("2d");
-
-    context.font = "30px sans";
-    context.fillStyle = "white";
-    context.textAlign = "center";
-
-    let string = `오늘의만찬은`;
-
-    let angle = Math.PI * 0.65;
-    let radius = 200;
-
-    context.translate(180, 280);
-    context.rotate(-1.1 * angle / 2);
-
-    for (let i = 0; i < string.length; i++) {
-
-        context.rotate(angle / string.length);
-        context.save();
-        context.translate(0, -1 * radius);
-        context.fillText(string[i], 0, 0);
-        context.restore();
-    }
-};
 
 $(document).ready(function(){
     if(document.querySelector(".food1")){
